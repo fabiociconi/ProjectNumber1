56df5e59237dfe24a298ae545a0f3017546fc55d
Fabio Ciconi <fabiociconi@yahoo.com.br>
2018-08-10 18:44:19 -0400

    Xam.Plugins.Settings
    fXam.Plugins.Settings

--- ProjectNumber1/ProjectNumber1/Helpers/Settings.cs
+++ ProjectNumber1/ProjectNumber1/Helpers/Settings.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,58 +12,59 @@ namespace ProjectNumber1.Helpers
         ///
 
         ///
-        //private static ISettings AppSettings
-        //{
-        //    get
-        //    {
-        //        return CrossSettings.Current;
-        //    }
-        //}
-
-
-        //#region Public Variables
-        //public static string Email
-        //{
-        //    get
-        //    {
-        //        return AppSettings.GetValueOrDefault("Email", "");
-        //    }
-        //    set
-        //    {
-        //        AppSettings.AddOrUpdateValue("Email", value);
-        //    }
-        //}
-        //public static string Password
-        //{
-        //    get
-        //    {
-        //        return AppSettings.GetValueOrDefault("Password", "");
-        //    }
-        //    set
-        //    {
-        //        AppSettings.AddOrUpdateValue("Password", value);
-        //    }
-        //}
-        //public static string AccessToken
-        //{
-        //    get
-        //    {
-        //        return AppSettings.GetValueOrDefault("AccessToken", "");
-        //    }
-        //    set
-        //    {
-        //        AppSettings.AddOrUpdateValue("AccessToken", value);
-        //    }
-        //}
-        //public static DateTime AccessTokenExpirationDate
-        //{
-        //    get
-        //    {
-        //        return AppSettings.GetValueOrDefault("AccessTokenExpirationDate", DateTime.UtcNow);
-        //    }
-        //    set
-        //    {
-        //        AppSettings.AddOrUpdateValue("AccessTokenExpirationDate", value);
-        //    }
+        private static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
         }
+
+
+       
+        public static string Email
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault("Email", "");
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue("Email", value);
+            }
+        }
+        public static string Password
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault("Password", "");
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue("Password", value);
+            }
+        }
+        public static string AccessToken
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault("AccessToken", "");
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue("AccessToken", value);
+            }
+        }
+        public static DateTime AccessTokenExpirationDate
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault("AccessTokenExpirationDate", DateTime.UtcNow);
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue("AccessTokenExpirationDate", value);
+            }
+        }
+    }
 }
